417c0c8c8a3af69a5d99ee2a8d4dddd176d5b413
pya-h <p4ya.gcc@gmail.com>
2025-01-10 00:55:06 +0330

    Add .env input for File Separator String - Also wrote a try_reading_string function to simplify & ensure getting inputs, and also prevent unwrap crashes.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 <h2>RUST</h2>
 <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; * Simple app for hiding and extracting important files inside another file, usually images.</p>
 
+<h2>TODOs</h2>
+<ul>
+    <li>Multi-Secret Files inside image feature</li>
+    <li>Extract secret files by their actual name</li>
+</ul>
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, io};
-
-const SEPARATOR: &[u8] = "###$$$!!!WHO-KNOWS-WHAT-COMES-NEXT!!!$$$###".as_bytes();
+use dotenv::dotenv;
+use std::env;
 
 #[macro_export] macro_rules! concat_bytes {
         ($($x:expr), *) => {
@@ -19,6 +19,25 @@ const SEPARATOR: &[u8] = "###$$$!!!WHO-KNOWS-WHAT-COMES-NEXT!!!$$$###".as_bytes(
          print!("\x07");
      };
  }
+
+fn try_reading_string(input_message: &str, target: &mut String) -> bool {
+    // Simple function which ensures getting valid input from user, and also allow user to cancel input.
+    let mut temp_input = String::new();
+    loop {
+        println!("{}", input_message);
+        let reading_result = io::stdin().read_line(&mut temp_input);
+        if let Err(err) = reading_result {
+            println!("Invalid Path Idiot!: {}\nEnter empty input to cancel ...", err.to_string());
+            continue;
+        }
+        *target = temp_input.to_string();
+        if target.trim().len() == 0 {
+            break;
+        }
+        return true;
+    }
+    false
+}
 fn read_file_bytes(filename: &str) -> Vec<u8> {
     match fs::read(filename) {
         Ok(bytes) => bytes,
@@ -26,11 +45,11 @@ fn read_file_bytes(filename: &str) -> Vec<u8> {
         Err(err) => panic!("{}", err),
     }
 }
-fn hide_secret_file(image_path: &str, secret_file_path: &str, output_path: &str) -> Result<(), io::Error> {
+fn hide_secret_file(image_path: &str, secret_file_path: &str, output_path: &str, file_separator_bytes: &[u8]) -> Result<(), io::Error> {
     let image_data = read_file_bytes(image_path);
     let secret_file_path = read_file_bytes(secret_file_path);
 
-    let output_data = concat_bytes!(image_data, SEPARATOR, secret_file_path);
+    let output_data = concat_bytes!(image_data, file_separator_bytes, secret_file_path);
     fs::write(output_path, output_data)
 }
 
@@ -43,16 +62,16 @@ fn extract_secret_file(bytes: &Vec<u8>, start: usize, end: usize, file_number: u
     fs::write(format!("secret_x_{}", file_number), &bytes[start..end])
 }
 
-fn start_extracting_secret_files(bytes: &Vec<u8>, mut start_index: usize) -> usize {
+fn start_extracting_secret_files(bytes: &Vec<u8>, mut start_index: usize, file_separator_bytes: &[u8]) -> usize {
     let mut i = start_index;
     let length = bytes.len();
-    let separator_length = SEPARATOR.len();
+    let separator_length = file_separator_bytes.len();
     let mut separator_index = 0;
     let mut files_extracted = 0;
 
     while i < length {
         if separator_index < separator_length {
-            if &bytes[i] == &SEPARATOR[separator_index] {
+            if &bytes[i] == &file_separator_bytes[separator_index] {
                 separator_index += 1;
             } else {
                 separator_index = 0;
@@ -60,7 +79,7 @@ fn start_extracting_secret_files(bytes: &Vec<u8>, mut start_index: usize) -> usi
         } else {
             match extract_secret_file(&bytes, start_index, i - separator_length, files_extracted + 1) {
                 Err(err) => {
-                    println!("Error ectracting file: {}", err.to_string())
+                    println!("Error extracting a file, skipping its data; Reason: {}", err.to_string())
                 }
                 _ => {
                     files_extracted += 1;
@@ -74,7 +93,7 @@ fn start_extracting_secret_files(bytes: &Vec<u8>, mut start_index: usize) -> usi
     }
     match extract_secret_file(&bytes, start_index, length, files_extracted + 1) {
         Err(err) => {
-
+            println!("Error writing extracted data inside a new file; Reason: {}", err.to_string())
         }
         _ => {
             files_extracted += 1;
@@ -84,8 +103,8 @@ fn start_extracting_secret_files(bytes: &Vec<u8>, mut start_index: usize) -> usi
     files_extracted
 }
 
-fn process_combined_file(filename: &str) -> Result<(), String> {
-    let separator_length = SEPARATOR.len();
+fn process_combined_file(filename: &str, file_separator_bytes: &[u8]) -> Result<(), String> {
+    let separator_length = file_separator_bytes.len();
     match fs::read(filename)  {
         Ok(bytes) => {
             let mut separator_index = 0;
@@ -94,13 +113,13 @@ fn process_combined_file(filename: &str) -> Result<(), String> {
 
             while i < bytes_length {
                 if separator_index < separator_length {
-                    if &bytes[i] == &SEPARATOR[separator_index] {
+                    if &bytes[i] == &file_separator_bytes[separator_index] {
                         separator_index += 1;
                     } else {
                         separator_index = 0;
                     }
                 } else {
-                    if start_extracting_secret_files(&bytes, i) == 0 {
+                    if start_extracting_secret_files(&bytes, i, &file_separator_bytes) == 0 {
                         return Err(String::from("Found some secret files but couldn't extract them."));
                     }
                     break;
@@ -118,30 +137,30 @@ fn process_combined_file(filename: &str) -> Result<(), String> {
     Ok(())
 }
 fn main() {
+    dotenv().ok();
+    let file_separator_string = env::var("FILE_SEPARATOR_TEXT")
+        .expect("FILE_SEPARATOR_TEXT is not set! It must be set and never change.");
+    let file_separator_bytes = file_separator_string.as_bytes();
     beep!();
     loop {
         let mut operation = String::new();
-        println!("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-        println!("SELECT OPERATION:\n\t[H]IDE\n\t[E]XTRACT\n\t[Q]UIT");
-        println!("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-        io::stdin().read_line(&mut operation).unwrap();
+        !try_reading_string(
+            "- - - - - - - - - - - - - - - - - - - - - - - - - - - - -\nSELECT OPERATION:\n\t[H]IDE\n\t[E]XTRACT\n\t[Q]UIT\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - -",
+                         &mut operation);
+
         match operation.trim() {
             "H" | "h" => {
                 let mut image_path = String::new();
                 let mut secret_file_path = String::new();
-
-                println!("Image Path: ");
-                io::stdin().read_line(&mut image_path).unwrap();
-                println!("File Path: ");
-                io::stdin().read_line(&mut secret_file_path).unwrap();
-
                 let mut output_path = String::new();
-                println!("Output Path: ");
-                io::stdin().read_line(&mut output_path).unwrap();  // TODO: It's better to automatically put the image extension at the end of the filename,
-                // since user may enter wrong or mismatched extension
 
+                if !try_reading_string("Image Path: ", &mut image_path)
+                    || !try_reading_string("Secret File Path: ", &mut secret_file_path)
+                    || !try_reading_string("Output Path: ", &mut output_path) {
+                    continue;
+                }
                 println!("Processing ...");
-                match hide_secret_file(&image_path.trim(), &secret_file_path.trim(), &output_path.trim()) {
+                match hide_secret_file(&image_path.trim(), &secret_file_path.trim(), &output_path.trim(), &file_separator_bytes) {
                     Ok(()) => { beep!(); println!("Successfully hid your requested file inside the image."); },
                     Err(err) => println!("FUCK! Failed to hide your requested file:\tReason:\n{}", err),
                 };
@@ -149,15 +168,17 @@ fn main() {
             "E" | "e" => {
                 let mut combined_file_path = String::new();
 
-                println!("File Path: ");
-                io::stdin().read_line(&mut combined_file_path).unwrap(); // TODO: Replace these .unwrap and panic usages too, since they crash app thread
-                if let Err(err) = process_combined_file(combined_file_path.trim()) {
+                if !try_reading_string("Combined [By Me] File Path: ", &mut combined_file_path) {
+                    continue;
+                }
+                println!("{}", combined_file_path.trim());
+                if let Err(err) = process_combined_file(combined_file_path.trim(), &file_separator_bytes) {
                     println!("FUCK! {}", err);
                 } else {
                     beep!();
                 }
             }
-            "Q" | "q" => {
+            "Q" | "q" | "" => {
                 beep!();
                 println!("FUCK U & HAVE A NICE DAY.");
                 break;
